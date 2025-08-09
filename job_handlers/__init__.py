6111c208cbcf038d5122202c7be12310fd6a2be6
Adam Cross <adamfgcross@gmail.com>
2025-08-09 14:20:02 -0400

    add __init__.py files to create python modules

--- job_handlers/__init__.py
+++ job_handlers/__init__.py
@@ -1,7 +1,7 @@
 from .compress_video import handle as compress_video_handler
 
 job_registry = {
-    "compress_video": compress_video_handler
+    "compress-video": compress_video_handler
 }
 
 def get_handler_for_type(job_type):
